8ef62c6c2482fcd78831bf2ccb761f2e43365586
Ryszard Tuora <ryszardtuora@gmail.com>
2021-05-26 14:32:42 +0200

    cleaning

--- workspace/api.py
+++
@@ -1,8 +0,0 @@
-import requests
-import json
-
-address = "https://corona.lmao.ninja/v2/countries/Poland?yesterday=true&strict=true&query "
-response = requests.get(address)
-data = json.loads(response.content)
-
-print(f"Yesterday {data['todayCases']} people got sick, and {data['todayDeaths']} people died because of the coronavirus pandemic")
